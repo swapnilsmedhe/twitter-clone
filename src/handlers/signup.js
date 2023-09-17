1a342133770224aff5bebf934796f0420b2297f1
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2023-09-17 18:00:02 +0530

    Remove unwanted console log

--- src/handlers/signup.js
+++ src/handlers/signup.js
@@ -2,7 +2,6 @@ const createSignupHanlder = (neo4jSession) => {
   return (req, res) => {
     neo4jSession.executeWrite((tx) => {
       const { username, password } = req.body;
-      console.log(username, password);
       tx.run(
         `CREATE(user:User {name: "${username}", password: "${password}"})`
       );
